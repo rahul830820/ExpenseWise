c417de7f8411f3c735b8c440b346c1d03f0d2314
Rahul Kamble <rahulkamble@Rahuls-MacBook-Air.local>
2026-06-29 19:13:01 +0530

    added period for monthly category wise and top categories

--- app/api/reports.py
+++ app/api/reports.py
@@ -6,7 +6,7 @@ from app.models.user import User
 from app.core.dependencies import get_current_user
 
 from app.schemas.report import CategoryWiseReport, MonthlyReport, TopCategoryReport
-
+from app.schemas.dashboard_period import DashboardPeriod
 from app.services.report_service import (
     get_monthly_report,
     get_top_categories,
@@ -18,20 +18,38 @@ router = APIRouter(prefix="/reports", tags=["Reports"])
 
 @router.get("/category-wise", response_model=list[CategoryWiseReport])
 def category_wise_report(
-    db: Session = Depends(get_db), current_user: User = Depends(get_current_user)
+    period: DashboardPeriod = DashboardPeriod.MONTH,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user),
 ):
-    return get_category_wise_report(db=db, current_user=current_user)
+    return get_category_wise_report(
+        db=db,
+        current_user=current_user,
+        period=period,
+    )
 
 
 @router.get("/monthly", response_model=list[MonthlyReport])
 def monthly_report(
-    db: Session = Depends(get_db), current_user: User = Depends(get_current_user)
+    period: DashboardPeriod = DashboardPeriod.MONTH,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user),
 ):
-    return get_monthly_report(db=db, current_user=current_user)
+    return get_monthly_report(
+        db=db,
+        current_user=current_user,
+        period=period,
+    )
 
 
 @router.get("/top-categories", response_model=list[TopCategoryReport])
 def top_categories_report(
-    db: Session = Depends(get_db), current_user: User = Depends(get_current_user)
+    period: DashboardPeriod = DashboardPeriod.MONTH,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user),
 ):
-    return get_top_categories(db=db, current_user=current_user)
+    return get_top_categories(
+        db=db,
+        current_user=current_user,
+        period=period,
+    )
--- app/services/report_service.py
+++ app/services/report_service.py
@@ -4,60 +4,119 @@ from sqlalchemy.orm import Session
 from app.models.expense import Expense
 from app.models.category import Category
 from app.models.user import User
+from app.schemas.dashboard_period import DashboardPeriod
+from app.utils.date_filters import get_period_dates
 
+def get_category_wise_report(
+    db: Session,
+    current_user: User,
+    period: DashboardPeriod,
+):
 
-def get_category_wise_report(db: Session, current_user: User):
-
-    results = (
-        db.query(
-            Category.name.label("category"), func.sum(Expense.amount).label("total")
-        )
-        .join(Expense, Expense.category_id == Category.id)
-        .filter(Expense.user_id == current_user.id)
-        .group_by(Category.name)
-        .all()
+    start_date, end_date = get_period_dates(period)
+    category_query = (
+    db.query(
+        Category.name.label("category"),
+        func.sum(Expense.amount).label("total"),
     )
+    .join(
+        Expense,
+        Expense.category_id == Category.id,
+    )
+    .filter(
+        Expense.user_id == current_user.id
+    )
+)
+    
+    if start_date:
+        category_query = category_query.filter(
+            Expense.expense_date >= start_date,
+            Expense.expense_date <= end_date,
+        )
+    results = (
+    category_query
+    .group_by(Category.name)
+    .all()
+)
 
     return results
 
 
-def get_monthly_report(db: Session, current_user: User):
+def get_monthly_report(
+    db: Session,
+    current_user: User,
+    period: DashboardPeriod,
+):
+    
+    start_date, end_date = get_period_dates(period)
+
+    monthly_query = (
+    db.query(
+        extract("year", Expense.expense_date).label("year"),
+        extract("month", Expense.expense_date).label("month"),
+        func.sum(Expense.amount).label("total"),
+    )
+    .filter(
+        Expense.user_id == current_user.id
+    )
+)
+    
+    if start_date:
+        monthly_query = monthly_query.filter(
+            Expense.expense_date >= start_date,
+            Expense.expense_date <= end_date,
+        )
 
     results = (
-        db.query(
-            extract("year", Expense.expense_date).label("year"),
-            extract("month", Expense.expense_date).label("month"),
-            func.sum(Expense.amount).label("total"),
-        )
-        .filter(Expense.user_id == current_user.id)
-        .group_by(
-            extract("year", Expense.expense_date),
-            extract("month", Expense.expense_date),
-        )
-        .order_by(
-            extract("year", Expense.expense_date),
-            extract("month", Expense.expense_date),
-        )
-        .all()
+    monthly_query
+    .group_by(
+        extract("year", Expense.expense_date),
+        extract("month", Expense.expense_date),
     )
-
+    .order_by(
+        extract("year", Expense.expense_date),
+        extract("month", Expense.expense_date),
+    )
+    .all()
+)
     return [
         {"month": f"{int(r.year)}-{int(r.month):02d}", "total": float(r.total)}
         for r in results
     ]
 
 
-def get_top_categories(db: Session, current_user: User):
+def get_top_categories(
+    db: Session,
+    current_user: User,
+    period: DashboardPeriod,
+):
+    start_date, end_date = get_period_dates(period)
+    top_category_query = (
+    db.query(
+        Category.name.label("category"),
+        func.sum(Expense.amount).label("total"),
+    )
+    .join(
+        Expense,
+        Expense.category_id == Category.id,
+    )
+    .filter(
+        Expense.user_id == current_user.id
+    )
+)
+
+    if start_date:
+        top_category_query = top_category_query.filter(
+            Expense.expense_date >= start_date,
+            Expense.expense_date <= end_date,
+        )
 
     results = (
-        db.query(
-            Category.name.label("category"), func.sum(Expense.amount).label("total")
-        )
-        .join(Expense, Expense.category_id == Category.id)
-        .filter(Expense.user_id == current_user.id)
-        .group_by(Category.name)
-        .order_by(func.sum(Expense.amount).desc())
-        .all()
+    top_category_query
+    .group_by(Category.name)
+    .order_by(
+        func.sum(Expense.amount).desc()
     )
-
+    .all()
+)
     return results
